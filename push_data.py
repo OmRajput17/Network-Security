56cfedff1e883a3344633579220fc40812e234da
Om Rajput <rajputom2408@gmail.com>
2025-06-10 10:20:03 +0530

    ETL Pipeline Implemented

---
+++ push_data.py
@@ -0,0 +1,64 @@
+import os
+import sys
+import json
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_DB_URL = os.getenv('MONGO_DB_URL')
+
+import certifi
+ca = certifi.where()
+
+import pandas as pd
+import numpy as np
+import pymongo
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+
+class NetworkDataExtract:
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+    
+    def csv_to_json_converter(self, filepath):
+        try:
+            data = pd.read_csv(filepath)
+            data.reset_index(drop=True, inplace=True)
+            records = list(json.loads(data.T.to_json()).values())
+            return records
+
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+    def insert_data_mongodb(self, records, database, collection):
+        try:
+            self.database = database
+            self.records = records
+            self.collection = collection
+
+            self.mongo_client = pymongo.MongoClient(MONGO_DB_URL)
+            self.database = self.mongo_client[self.database]
+
+            self.collection = self.database[self.collection]
+            self.collection.insert_many(self.records)
+
+            return (len(self.records))
+
+
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+if __name__ == '__main__':
+    FILE_PATH = 'NewtorkData\phisingData.csv'
+    DATA_BASE = 'NETWORK'
+    Collection = 'NewtorkData'
+    networkobj = NetworkDataExtract()
+    records = networkobj.csv_to_json_converter(filepath=FILE_PATH)
+    print(records)
+    no_of_records = networkobj.insert_data_mongodb(records, DATA_BASE, Collection)
+    print(no_of_records)
