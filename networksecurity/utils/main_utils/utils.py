2e8a1fb80f17caa1cf2e1d302c0d90a84486e89b
Om Rajput <rajputom2408@gmail.com>
2025-06-15 11:23:38 +0530

    data validation completed

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -1,6 +1,6 @@
 import os,sys
 import yaml
-import dill
+# import dill
 from pathlib import Path
 from networksecurity.logging.logger import logging
 import numpy as np
@@ -13,6 +13,17 @@ def read_yaml(file_path: str) -> dict:
     try:
         with open(file_path, "rb") as yaml_file:
             return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+    
+def write_yaml_file(file_path: str, content: object, replace: bool = False)-> None:
+    try:
+        if replace:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        os.makedirs(os.path.dirname(file_path), exist_ok = True)
+        with open(file_path, "w") as file:
+            yaml.dump(content, file)
     except Exception as e:
         raise NetworkSecurityException(e, sys)
 
